031443f44dbd518a4cc8fae353e48b6e2a4836b7
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-27 08:21:33 +0300

    Add files via upload

---
+++ test/scoros.py
@@ -0,0 +1,20 @@
+with open('file1.txt', 'r', encoding="utf-8") as file1:
+    with open('file2.txt', 'r', encoding="utf-8") as file2:
+        different = set(file1).difference(file2)
+
+different.discard('\n')
+
+with open('result1.txt', 'w') as file_out:
+    for line in different:
+        file_out.write(line)
+
+
+with open('file2.txt', 'r', encoding="utf-8") as file2:
+    with open('file1.txt', 'r', encoding="utf-8") as file1:
+        different = set(file2).difference(file1)
+
+different.discard('\n')
+
+with open('result2.txt', 'w') as file_out:
+    for line in different:
+        file_out.write(line)
